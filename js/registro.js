af4d77a36d9e15683ce2c91362f7a8681ddf6c8a
Denny K. Schuldt <dschuldt@espol.edu.ec>
2013-07-06 11:04:24 -0500

    Registro responsive
    Ahora la página de registro es responsive, y también se han terminado de
    implementar las funcionalidades para calcular el IMC, el peso ideal y
    cuantos kilos debe ganar o perder el usuario (Depende de si es hombre o
    mujer).

--- js/registro.js
+++ js/registro.js
@@ -1,5 +1,13 @@
 
-    var lis = document.querySelectorAll('#main_nav li');    
+    var lis = document.querySelectorAll('#main_nav li');
+    var imc = "";
+    var peso_dado = "";
+    var peso_ideal = "";
+    var ganar_perder = "";
+    var hombre = false, mujer = false;
+    var sexo = false;
+    var peso_checked = false;
+    var estatura_checked = false;
 
     /*
 	 * Description:
@@ -65,34 +73,89 @@
         estatura = document.getElementById("txtEstatura");
         estatura.addEventListener("input", validar, false);
 		
-
-        $("#lb, #kg, #m, #cm").change(function(){
-            calcularIMC();
+        $("#Masculino, #Femenino").change(function () {
+            if ($('#Femenino').is(':checked')) mujer = true;
+            if ($('#Masculino').is(':checked')) hombre = true;
+            sexo = true;
         });
 
-		$('#txtEstatura').keyup(function (){		    
-		    if ($.trim($("#txtPeso").val())) {		        
-		        calcularIMC();
-		    }
+        $("#lb, #kg").change(function () {
+            peso_checked = true;
+            if (sexo) {
+                if(estatura_checked){
+                    calcularIMC();
+                    calcularPesoIdeal();
+                }
+            }
+            else alert("Debe especificar un sexo");
+        });
+
+        $("#m, #cm").change(function () {
+            estatura_checked = true;
+            if (sexo) {
+                if (peso_checked) {
+                    calcularIMC();
+                    calcularPesoIdeal();
+                }
+            }
+            else alert("Debe especificar un sexo");
+        });
+
+        $('#txtEstatura').keyup(function () {
+            if ($.trim($("#txtPeso").val())) {
+                if (sexo) {
+                    if(peso_checked && estatura_checked){
+                        calcularIMC();
+                        calcularPesoIdeal();
+                    }
+                }
+                else alert("Debe especificar un sexo");
+            }            
 		});
 
-		$('#txtPeso').keyup(function () {
-		    if ($.trim($("#txtEstatura").val())) {
-		        calcularIMC();
-		    }
+        $('#txtPeso').keyup(function () {
+            if ($.trim($("#txtEstatura").val())) {
+                if (sexo) {
+                    if(peso_checked && estatura_checked){
+                        calcularIMC();
+                        calcularPesoIdeal();
+                    }
+                }
+                else alert("Debe especificar un sexo");
+            }
 		});
-
 		validar();
 	}
-	
-	function calcularIMC() {
-	    var p = parseFloat($("#txtPeso").val()).toFixed(2);
-	    var e = parseFloat($("#txtEstatura").val()).toFixed(2);
-	    if (document.getElementById('lb').checked) p = (p / 2.2).toFixed(2);
-	    if (document.getElementById('cm').checked) e = (e / 100).toFixed(2);
-	    e = (e * e).toFixed(2);
-	    var valor = (p / e).toFixed(2);
-	    document.querySelector('#imc').innerHTML = valor + " kg/m<sup>2</sup>";
+
+    function calcularIMC(){
+        peso_dado = parseFloat($("#txtPeso").val()).toFixed(2);
+        var e = parseFloat($("#txtEstatura").val()).toFixed(2);
+        if (document.getElementById('lb').checked) peso_dado = (peso_dado / 2.2).toFixed(2);
+        if (document.getElementById('cm').checked) e = (e / 100).toFixed(2);
+        e = (e * e).toFixed(2);
+        imc = (peso_dado / e).toFixed(2);
+        document.querySelector('#imc').innerHTML = imc + " kg/m<sup>2</sup>";
+    }	
+
+	function calcularPesoIdeal() {
+	    var imci = 0;
+	    if(mujer) imci = 22;
+	    if(hombre) imci = 26;
+	    peso_ideal = ((imci) * (peso_dado / imc)).toFixed(2);
+	    document.querySelector('#ideal').innerHTML = peso_ideal + " kg";
+	    if (peso_dado > peso_ideal) {
+	        document.querySelector('#resultado').innerHTML = "perder ";
+	        document.querySelector('#resultado').style.color = "red";
+	        document.querySelector('#kilos').innerHTML = (peso_dado - peso_ideal).toFixed(2) + " kg";
+	    }
+	    else if (peso_ideal > peso_dado) {
+	        document.querySelector('#resultado').innerHTML = "ganar ";
+	        document.querySelector('#resultado').style.color = "green";
+	        document.querySelector('#kilos').innerHTML = (peso_ideal - peso_dado).toFixed(2) + " kg";
+	    }
+	    else if (peso_dado == peso_ideal) {	        
+	        document.querySelector('#ideal').innerHTML = "Mantenerse en forma. ;)";
+	    }
 	}
 
 	function validar(){
@@ -170,7 +233,7 @@
 		  peso.setCustomValidity('Ingresar su Peso');
 		  peso.style.background='rgb(217,226,119)';
 		}
-		else{
+		else {
 		  peso.setCustomValidity('');
 		  peso.style.background='#FFFFFF';
 		}
