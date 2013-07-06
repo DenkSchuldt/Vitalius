480daf3ee5e886fc0d3de9a09c601afb203ae46b
Jacqueline Ramos <jackie.ljrm@gmail.com>
2013-07-06 15:23:47 -0500

    Cambiando pagina quebeber con su respectivo css

--- js/que_beber.js
+++ js/que_beber.js
@@ -34,6 +34,45 @@
 				},true);		
 			}
 		}
+		seccion = document.getElementById('nutritivoSection');
+		cargarBajarDePeso();
+	}
+	
+	function cargarBajarDePeso(){
+		var url="../xml/bebidasBajarDePeso.xml";
+		var request= new XMLHttpRequest();
+		
+		request.addEventListener('load',procesarBajarDePeso,false);
+		request.open("GET",url,true);
+		request.send(null);
+	}
+	
+	function procesarBajarDePeso(e){
+	
+		var xml= e.target.responseXML;
+		var descripcion = xml.documentElement.getElementsByTagName("bebida");
+		//numNot=receta.length;
+		for(i=0;i<descripcion.length;i++){
+		
+			titulo = descripcion[i].getElementsByTagName("titulo")[0].textContent;
+			receta = descripcion[i].getElementsByTagName("receta")[0].textContent;
+			
+			article = document.createElement('article');
+			h3 = document.createElement('h3');
+			h4 = document.createElement('h4');
+			p = document.createElement('p');
+			
+			texto = document.createTextNode(titulo);
+			h3.appendChild(texto);
+			texto = document.createTextNode(receta);
+			p.appendChild(texto);
+						
+			article.appendChild(h3);
+			article.appendChild(p);
+		
+			seccion.appendChild(article);
+		}
+
 	}
 	
 	window.onload = queBeberStart();
